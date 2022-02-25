3a3d948c56df80cf0905585d7e71bfb4726f6fb3
whitebear <woojs0505@naver.com>
2022-02-25 12:25:40 +0900

    init

---
+++ src/main/java/com/moreversal/grabthathoe/GrabthathoeApplication.java
@@ -0,0 +1,13 @@
+package com.moreversal.grabthathoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrabthathoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrabthathoeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moreversal/grabthathoe/GrabthathoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moreversal.grabthathoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrabthathoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
