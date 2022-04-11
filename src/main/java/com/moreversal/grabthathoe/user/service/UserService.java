b62654c840e2a855a317a7c0d68c5326ad6d509a
우준성 <woojs0505@naver.com>
2022-04-11 17:29:17 +0900

    :memo: :: gitignore and UserRepository

--- src/main/java/com/moreversal/grabthathoe/user/service/UserService.java
+++ src/main/java/com/moreversal/grabthathoe/user/service/UserService.java
@@ -1,7 +1,8 @@
 package com.moreversal.grabthathoe.user.service;
 
+import com.moreversal.grabthathoe.user.domain.dto.UserJoinDto;
 import com.moreversal.grabthathoe.user.domain.entity.User;
 
 public interface UserService {
-    User join();
+    User join(UserJoinDto userJoinDto);
 }
--- src/main/java/com/moreversal/grabthathoe/user/service/UserServiceImpl.java
+++ src/main/java/com/moreversal/grabthathoe/user/service/UserServiceImpl.java
@@ -6,13 +6,20 @@ import com.moreversal.grabthathoe.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityExistsException;
+
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
+    @Override
     public User join(UserJoinDto userJoinDto) {
+        if(userRepository.existsUserByPhone(userJoinDto.getPhone())) {
+            throw new EntityExistsException();
+        }
+
         User user = User.builder()
                 .phone(userJoinDto.getPhone())
                 .name(userJoinDto.getName())
