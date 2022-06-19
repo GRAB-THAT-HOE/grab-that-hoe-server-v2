7b0765729b693a3add900116a6872875a09855b2
우준성 <woojs0505@naver.com>
2022-06-19 21:47:17 +0900

    :memo: :: token lib 작성

--- src/main/java/com/moreversal/grabthathoe/common/lib/Jwt.java
+++ src/main/java/com/moreversal/grabthathoe/common/lib/Jwt.java
@@ -5,13 +5,16 @@ import com.moreversal.grabthathoe.common.exception.AuthorizationException;
 import com.moreversal.grabthathoe.user.domain.entity.User;
 import com.moreversal.grabthathoe.user.domain.repository.UserRepository;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.spec.SecretKeySpec;
+import javax.servlet.http.HttpServletRequest;
 import javax.xml.bind.DatatypeConverter;
 import java.security.Key;
 import java.util.*;
@@ -30,6 +33,7 @@ public class Jwt {
     private final UserRepository userRepository;
 
     public String createToken(User user, JwtType jwtType) {
+
         Date nowDate = new Date();
         Calendar expiredDate = Calendar.getInstance();
         expiredDate.setTime(nowDate);
@@ -56,17 +60,49 @@ public class Jwt {
         claimsMap.put("id", user.getId());
         claimsMap.put("phone", user.getPhone());
 
+        JwtBuilder builder = Jwts.builder().setHeaderParams(headerMap)
+                .setClaims(claimsMap)
+                .setExpiration(expiredDate.getTime())
+                .signWith(signatureAlgorithm, signingKey);
 
-
-        return null;
+        return builder.compact();
     }
 
     public User validateToken(String token) {
+
         Claims claims = Jwts.parser().setSigningKey(DatatypeConverter.parseBase64Binary(secretAccessKey)).parseClaimsJws(token).getBody();
         Optional<User> user = userRepository.getUserByPhone(claims.get("phone", String.class));
+
         if(!user.isPresent()) {
             throw new AuthorizationException("해당하는 유저 정보가 없습니다.");
         }
+
         return user.get();
+    }
+
+    public String refresh(String refreshToken) {
+
+        Claims claims = Jwts.parser().setSigningKey(DatatypeConverter.parseBase64Binary(secretRefreshKey)).parseClaimsJws(refreshToken).getBody();
+        Optional<User> user = userRepository.getUserByPhone(claims.get("phone", String.class));
+
+        if(!user.isPresent()) {
+            throw new AuthorizationException("해당하는 유저 정보가 없습니다.");
+        }
+
+        return createToken(user.get(), JwtType.ACCESS);
+    }
+
+    public String extract(HttpServletRequest request, String type) {
+
+        Enumeration<String> headers = request.getHeaders("Authorization");
+
+        while(headers.hasMoreElements()) {
+            String value = headers.nextElement();
+            if(value.toLowerCase().startsWith(type.toLowerCase())) {
+                return value.substring(type.length()).trim();
+            }
+        }
+
+        return Strings.EMPTY;
     }
 }
